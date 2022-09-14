540bfda060612376416c8d52d37e04168bc6eb5d
PegosStelios <stelios@pegos.net>
2022-09-15 00:46:16 +0300

    Edited Models

--- app/Models/Click.php
+++ app/Models/Click.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Click extends Model
-{
+class Click extends Model {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function listing() {
+        return $this->belongsTo(Listing::class);
+    }
 }
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,7 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Listing extends Model
-{
+class Listing extends Model {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function clicks(){
+        return $this->hasMany(Click::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function tag(){
+        return $this->belongsToMany(Tag::class);
+    }
 }
